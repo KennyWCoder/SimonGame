1a0c7aad7bd2a010351f34da605b03eca11a9a84
Kenny Wai <97656734+KennyWCoder@users.noreply.github.com>
2022-04-11 22:20:37 -0700

    Update game.js

--- game.js
+++ game.js
@@ -38,7 +38,7 @@ function checkAnswer(currentLevel){
     setTimeout(function() {
       $("body").removeClass("game-over");
     }, 200);
-    $("#level-title").text("Game Over, Press Any Key to Restart");
+    $("#level-title").text("Game Over, Press Any Key or Click Any Space to Restart");
     startOver();
     console.log("wrong");
   }
